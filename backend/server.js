a8d099cf6190b0cf97afafec06bad08cb0b771ca
parishalakhlan <parishalakhlan@gmail.com>
2025-08-25 23:24:37 +0530

    removed logger file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 
 // Import custom modules
-const logger = require("./utils/logger");
+
 const errorHandler = require("./utils/errorHandler");
 const authMiddleware = require("./middlewares/authMiddleware");
 
