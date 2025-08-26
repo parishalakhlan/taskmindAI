16e1e9fca5d5c0ea0c1360b06e9d97098587b5a5
parishalakhlan <parishalakhlan@gmail.com>
2025-08-26 11:13:47 +0530

    fixed auth files

--- frontend/src/components/layout/OTPInput.tsx
+++ frontend/src/components/layout/OTPInput.tsx
@@ -57,7 +57,9 @@ export const OTPInput = ({
       {Array.from({ length }).map((_, index) => (
         <input
           key={index}
-          ref={(el) => (inputRefs.current[index] = el)}
+          ref={(el) => {
+            inputRefs.current[index] = el;
+          }}
           type="text"
           inputMode="numeric"
           maxLength={1}
